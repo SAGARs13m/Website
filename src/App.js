33487c16ed6a62771aca8866e379717f059c148b
SAGARs13m <sagargunduboyina13@gmail.com>
2024-09-26 16:10:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/home';
+import About from '././pages/About/about';
+import Contact from '././pages/Contact/contact';
+import Courses from './pages/Courses/courses';
+import News from '././pages/News/news';
+//import Test from './pages/Test/test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/news" element={<News />} />
+          {/* Add more routes here as needed */}
+          <Route path="/" element={<Home />} /> {/* Default route */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App;
